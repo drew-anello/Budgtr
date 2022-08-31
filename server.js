80bd06950e7dc56f93e1a5dd8df69e4a72ac386b
Drew Anello <drew.anello@gmail.com>
2022-08-31 15:42:59 -0400

    Server running

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => { 
-res.send('working in port',port)
-})
+  res.send('working in port ' + port);
+});
 
-app.listen(port)
-
+app.listen(port, () => {
+  console.log('port is working');
+});
