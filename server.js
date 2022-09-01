40c3b091c84f67afbdc39aaab874c24742518f08
Drew Anello <drew.anello@gmail.com>
2022-08-31 21:00:19 -0400

    adds app.get app.post

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const budget = require('./models/budget.js')
 
-app.get('/', (req, res) => { 
-  res.send('working in port ' + port);
+app.get('/', (req, res) => {
+  res.send('bonjour')
+})
+
+app.get("/budgets", (req, res)=>{
+  res.render("budget_index.ejs", {
+      budget, bankAccount,
+  });
 });
 
-app.listen(port, () => {
-  console.log('port is working');
+app.get("/budgets/new", (req, res)=>{
+  res.render("new.ejs");
 });
+
+app.post("/budgets", (req, res)=>{
+  budget.push(req.body);
+  res.redirect("/budgets");
+})
+
+app.get("/budgets/:index", (req, res)=>{
+  res.render("budget_show.ejs", {
+   
+  });
+});
+
+app.listen(3000);
